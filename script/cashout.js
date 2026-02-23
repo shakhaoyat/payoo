2c3caccaa4ed93897dce842e20f7ee52f81c753a
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-02-23 06:11:45 +0600

    second commit

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener("click", function () {
+      //1- get the agent number and validate
+      //2- get the amount ,validate ,convert to number
+      //3- GET THE Current balance, validate,convert to number
+      //4- calculate new balace
+      //5-get the pin and varify
+      //5.1 true:: show aleart> set balance
+      //5.2 false:: show an error alert> return 
+});
